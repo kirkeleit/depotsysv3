59c21e036b86057b02db7895229591a4d68288e4
Thorbjørn Kirkeleit <thorbjorn@kirkeleit.net>
2019-07-15 10:15:31 +0200

    Fikset litt i menyen.

--- application/views/standard.php
+++ application/views/standard.php
@@ -3,9 +3,6 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <meta name="generator" content="">
   <title>DepotSYS</title>
 
   <!-- Bootstrap core CSS -->
@@ -31,14 +28,15 @@
   <link href="/css/standard.css" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand" href="#">DepotSYS</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDepotsys" aria-controls="navbarDepotsys" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarCollapse">
+  <a class="navbar-brand" href="#">DepotSYS</a>
+  <div class="collapse navbar-collapse" id="navbarDepotsys">
+    <a class="navbar-brand" href="#">DepotSYS</a>
     <ul class="navbar-nav mr-auto">
-
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarKomponenter" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Komponenter
@@ -61,12 +59,7 @@
           <!--<a class="dropdown-item" href="<?php echo site_url('vedlikehold/avviksliste'); ?>">Avvik</a>-->
 	</div> 
       </li>
-
     </ul>
-    <form class="form-inline mt-2 mt-md-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 
